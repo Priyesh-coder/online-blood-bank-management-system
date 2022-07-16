5f63db0dfeb53c83c809c21d33d6eed96f2029f3
Priyesh Kumar <priyeshjnv183@gmail.com>
2022-07-16 13:55:12 +0530

    Add files via upload

---
+++ layout/_footer.php
@@ -0,0 +1,10 @@
+<footer class="foter">
+            <b>Made By: <a href="https://www.linkedin.com/in/priyesh-kumar-5357a71b1/">Priyesh Kumar</a></b>
+            <br>
+            &COPY;2022
+</footer>
+
+
+
+</body>
+</html>
